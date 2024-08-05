edf02c112000000e90a3099f4356c45ea0b8217c
Garren Tan <garrentmh@gmail.com>
2024-08-05 14:22:05 +0800

    first commit

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { onMount, onDestroy } from "svelte";
+  import { FeedbackStore } from "../stores";
+  import { fade, scale } from "svelte/transition";
+  import FeedbackItem from "./FeedbackItem.svelte";
+  import FeedbackForm from "./FeedbackForm.svelte";
+  // let feedback = [];
+
+  // const unsubscribe = FeedbackStore.subscribe((data) => (feedback = data));
+
+  // onMount(() => {
+  //   console.log("mounted");
+  // });
+
+  // onDestroy(() => {
+  //   unsubscribe();
+  // });
+</script>
+
+{#each $FeedbackStore as fb (fb.id)}
+  <div in:fade={{ duration: 500 }} out:scale>
+    <FeedbackItem item={fb} on:delete />
+  </div>
+{/each}
---
+++ src/components/FeedbackStats.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { FeedbackStore } from "../stores";
+  export let count;
+  export let average;
+
+  $: count = $FeedbackStore.length;
+  $: average =
+    $FeedbackStore.reduce((a, { rating }) => a + rating, 0) / $FeedbackStore.length;
+</script>
+
+<div class="feedback-stats">
+  <h4>{count} Reviews</h4>
+  <h4>Ratings Average: {average}</h4>
+</div>
+
+<style>
+  .feedback-stats {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+    padding: 10px;
+    background-color: #202142;
+    border-radius: 5px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    font-size: 16px;
+    font-weight: bold;
+    color: #fff;
+  }
+</style>
